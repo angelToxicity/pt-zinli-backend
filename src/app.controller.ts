b079957643da89f84ab8ae85afb623a3693c81d3
Angel Rodriguez <klju784@gmail.com>
2024-09-06 02:31:14 -0400

    agregar conexion mongodb

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DbMongo } from './db';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private db:DbMongo) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  
+  @Get('db')
+    async getDb(): Promise<any> {
+    let conn = this.db.run();
+    let a = await conn.listCollections()
+    console.log(a)
+    // return this.db.run();
+    return "H";
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbMongo } from './db';
+import { MongooseModule } from '@nestjs/mongoose';
+import 'dotenv/config';
+const uri = process.env.MONGODB_STRING;
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(uri ? uri : "", {dbName: 'pt_zinli'})],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DbMongo],
 })
 export class AppModule {}
